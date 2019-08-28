5a1a7e1c2179d70f6662092c7d9dc9d7bf6b36ad
zero85258 <zero85258@gmail.com>
2019-08-29 01:53:50 +0800

    <>

--- analysis-package-manager.sh
+++ analysis-package-manager.sh
@@ -1,2 +1,2 @@
 # 分析brew $1=關鍵字
-curl -sX GET "https://formulae.brew.sh/api/formula.json" -H "accept: application/json" | jq '.[] | {st:(.name + " --- " + .desc) }' | grep $1 | less
+curl -sX GET "https://formulae.brew.sh/api/formula.json" -H "accept: application/json" | jq '.[] | (.name + " --- " + .desc)' | grep $1 | less
