6cb4c67098e33f5061d6a5c5fb15087a1c458d09
YuFuFu <zero85258@gmail.com>
2019-10-03 15:32:20 +0800

    Update check-s3.sh

--- check-s3.sh
+++ check-s3.sh
@@ -1,3 +1,11 @@
-#aws s3 ls --human-readable s3://XXXdata/flat_data/DMP/$1/
-#aws s3 ls --recursive --human-readable s3://XXXdata/sparktempdata/DMP/$1/
-aws s3 ls --recursive --human-readable s3://XXXdata/sparkmonthdata/DMP/$1/
+DATE_LEN=$(echo $1 | wc -c | awk '{print $1}')
+
+if [ ${DATE_LEN} -eq '7' ];then
+  aws s3 ls --recursive --human-readable s3://XXXdata/sparkmonthdata/DMP/$1/
+elif [ ${DATE_LEN} -eq '9' ];then
+  aws s3 ls --human-readable s3://XXXdata/flat_data/DMP/$1/
+  echo "----------------------------------------------------------------";
+  aws s3 ls --recursive --human-readable s3://XXXdata/sparktempdata/DMP/$1/
+else
+  echo "gag"
+fi
