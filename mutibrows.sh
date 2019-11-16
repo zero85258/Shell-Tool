c943ba805a5fa9f02a6e3330e744ddb8929a6301
YuFuFu <zero85258@gmail.com>
2019-11-16 12:58:11 +0800

    Update mutibrows.sh

--- mutibrows.sh
+++ mutibrows.sh
@@ -1,6 +1,1 @@
-echo "{"
-while read line
-do
-  echo curl -s line | w3m -T text/html -dump ;
-done < "${1:-/dev/stdin}"
-echo "}"
+${1:-/dev/stdin} | xargs -I {} curl -s {} | w3m -T text/html -dump | less
